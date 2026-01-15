92bdefe2e5d60073ec768d0ddcc2aaa84bac6274
yoanaml <yoana.mladenova00@gmail.com>
2026-01-15 03:47:35 +0200

    Add custom string functions

--- uno_project/uno_project.cpp
+++ uno_project/uno_project.cpp
@@ -4,7 +4,42 @@
 #include <iostream>
 #include <algorithm> 
 #include <random>    
-#include <ctime>     
+#include <ctime> 
+
+int streq(const char* a, const char* b) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] != b[i])
+            return 0;
+        i++;
+    }
+    return a[i] == b[i];
+}
+
+void strcp(char* dest, const char* src) {
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+void strconcat(char* dest, const char* src) {
+    int i = 0;
+    while (dest[i] != '\0') {
+        i++;
+    }
+    int j = 0;
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+}
 
 int main()
 {
